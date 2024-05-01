4fd7c56bf690743774c3241855c49f559c1f8a22
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-01 05:38:51 -0700

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that implements pascal triangle"""
+
+
+def pascal_triangle(n):
+    arr = []
+    for i in range(0, n):
+        arr2 = []
+        for j in range(0, i + 1):
+            if j == 0 or j == i:
+                arr2.append(1)
+            else:
+                arr2.append(arr[i - 1][j - 1] + arr[i - 1][j])
+        arr.append(arr2)
+    return arr
