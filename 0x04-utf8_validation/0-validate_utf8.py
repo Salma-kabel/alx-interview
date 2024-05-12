1571fb69a6e78fb538355d9ca3030bd17437cc4e
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-12 18:17:14 +0000

    task0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,8 +9,8 @@ def validUTF8(data):
     t = 1
     while i < len(data):
         if (data[i] & 0xf0) == 0xf0:
-            if i + 1 < len(data) and i + 2 < len(data) and i + 3 < len(data)
-            and (data[i + 1] & 0x80) == 0x80 and (data[i + 2] & 0x80) == 0x80
+            if i + 1 < len(data) and i + 2 < len(data) and i + 3 < len(data)\
+            and (data[i + 1] & 0x80) == 0x80 and (data[i + 2] & 0x80) == 0x80\
             and (data[i + 3] & 0x80) == 0x80:
                 t = 1
                 i += 4
@@ -18,7 +18,7 @@ def validUTF8(data):
                 t = 0
                 break
         elif (data[i] & 0xe0) == 0xe0:
-            if i + 1 < len(data) and i + 2 < len(data) and
+            if i + 1 < len(data) and i + 2 < len(data) and\
             (data[i + 1] & 0x80) == 0x80 and (data[i + 2] & 0x80) == 0x80:
                 t = 1
                 i += 3
