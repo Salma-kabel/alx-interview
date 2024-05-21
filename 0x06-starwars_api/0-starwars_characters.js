d06567667b07d64611be126e1a16c15ed5f59814
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-21 06:02:59 -0700

    task0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,25 +2,29 @@
 const request = require('request');
 const movie_id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movie_id
-request(url, function (err, response, body) {
+request(url, async function (err, response, body) {
   if (err) {
     console.log(err);
   } else { 
     const characters = JSON.parse(body).characters;
     for (let i = 0; i < characters.length; i++) {
-      print_characters(characters, i);
+      await print_characters(characters, i);
     }
   }
 });
 
 function print_characters (characters, i) {
-  const idlist = characters[i].split('/');
-  const id = idlist[idlist.length - 2];
-  request('https://swapi-api.alx-tools.com/api/people/' + id, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(JSON.parse(body).name);
-    }
+  return new Promise((resolve, reject) => {
+    const idlist = characters[i].split('/');
+    const id = idlist[idlist.length - 2];
+    request('https://swapi-api.alx-tools.com/api/people/' + id, function (err, response, body) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        console.log(JSON.parse(body).name);
+        resolve();
+      }
+    });
   });
 }
