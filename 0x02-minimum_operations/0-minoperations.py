206373d9a99cd26a3ec719969b0ea3f454af8cf6
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-13 00:12:56 +0000

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,6 +8,6 @@ def minOperations(n):
     to result in exactly n H characters in the file"""
     if n <= 1:
         return 0
-    for op in range(2, n+1):
-        if n % op == 0:
-            return minOperations(int(n/op)) + op
+    for operation in range(2, n+1):
+        if n % operation == 0:
+            return minOperations(int(n/operation)) + operation
